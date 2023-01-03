c3dd8e15536fcaecdecacb5f421eba892f7f934d
SinMinGyu <smgms7513@gmail.com>
2023-01-03 14:54:44 +0900

    practice_02_genericRecap

--- polymorphism.ts
+++ polymorphism.ts
@@ -1,10 +1,10 @@
 type SuperPrint = {
-    <T>(arr: T[]): T;
+    <T, M>(a: T[], b: M): T;
 };
 
-const superPrint: SuperPrint = (arr) => arr[0];
+const superPrint: SuperPrint = (a) => a[0];
 
-const a = superPrint([1, 2, 3, 4]);
-const b = superPrint([true, false, true]);
-const c = superPrint(["a", "b", "c"]);
-const d = superPrint([1, 2, true, "hello"]);
+const a = superPrint([1, 2, 3, 4], "x");
+const b = superPrint([true, false, true], 1);
+const c = superPrint(["a", "b", "c"], false);
+const d = superPrint([1, 2, true, "hello"], []);
